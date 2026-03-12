0bfed8c34522e8b126b5c9d08d59dcccf71c54ea
bernard <bernard@laurant.fr>
2026-03-12 19:51:12 +0100

    Replace text plus with circle-plus icon on Consommer button
    - Swap "+ Consommer" text prefix for an SVG circle-plus icon
    - Match the icon style used on the "Ajouter un réseau" button
    - Affected file: frontend/src/app/pages/consumptions.cljs

--- frontend/src/app/pages/consumptions.cljs
+++ frontend/src/app/pages/consumptions.cljs
@@ -26,7 +26,14 @@
            [:h2 "Mes consommations"]
            [:button.btn.btn--green.btn--small
             {:on-click #(rf/dispatch [:consumptions/create])}
-            "+ Consommer"]]
+            [:svg {:xmlns "http://www.w3.org/2000/svg" :width "16" :height "16"
+                   :viewBox "0 0 24 24" :fill "none" :stroke "currentColor"
+                   :stroke-width "2" :stroke-linecap "round" :stroke-linejoin "round"
+                   :style {:vertical-align "middle" :margin-right "4px"}}
+             [:circle {:cx "12" :cy "12" :r "10"}]
+             [:line {:x1 "12" :y1 "8" :x2 "12" :y2 "16"}]
+             [:line {:x1 "8" :y1 "12" :x2 "16" :y2 "12"}]]
+            "Consommer"]]
           (cond
             loading?
             [:p.loading "Chargement..."]
